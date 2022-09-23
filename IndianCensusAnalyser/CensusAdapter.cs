56d286be6944d90f62346ac50784a07a3f2ce064
Aj007ar <aj007.ar@gmail.com>
2022-09-23 18:08:48 +0530

    [Ajay] Added: Fetch state census data Program

---
+++ IndianCensusAnalyser/CensusAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser
+{
+    public class CensusAdapter
+    {
+        public string[] GetCensusData(string csvFilePath,string dataHeaders)
+        {
+            string[] censusData;
+            censusData = File.ReadAllLines(csvFilePath);
+            return censusData;
+        }
+    }
+}
