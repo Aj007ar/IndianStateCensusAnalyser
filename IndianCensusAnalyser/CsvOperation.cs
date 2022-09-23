4986fd2ffa5bab8dd0103936457b2d201e2e24b5
Aj007ar <aj007.ar@gmail.com>
2022-09-23 22:23:50 +0530

    [Ajay] Added: Check for invalid header Program

---
+++ IndianCensusAnalyser/CsvOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser
+{
+    public abstract class CsvOperations
+        {
+            public abstract string[] GetCensusData(string csvFilePath, string dataHeaders);
+        }
+    
+}
